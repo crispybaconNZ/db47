0d74a11c75431755c64f75e90a10b759d8e1f5bb
crispybaconNZ <crispin.anderton@gmail.com>
2020-10-03 18:03:02 +1300

    More
    Added Machine stand in with smoke stacks
    Player controlled with WASD
    Camera fixed

---
+++ Assets/Scripts/MouseCapture.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MouseCapture : MonoBehaviour {
+    [SerializeField] private GameObject _markerPrefab;
+    [SerializeField] private Camera _camera;
+
+    void Update() {
+
+        Ray ray;
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("Left mouse button clicked");
+            ray = _camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.collider.name == "Ground") {
+                    Vector3 pos = hit.point;
+                    Instantiate(_markerPrefab, pos, Quaternion.identity);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour {
+
+    [SerializeField] private CharacterController controller;
+    public float Speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    void Update() {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f) {
+            float speed = Speed;
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
+                speed = speed * 2f;
+            }
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
